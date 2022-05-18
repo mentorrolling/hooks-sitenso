b58e37dc6c0fd105ffd290e68d403232ac0eb45b
Pablo Marino <pmarino2013@gmail.com>
2022-05-18 13:52:41 -0300

    useReducer y useForm

--- src/components/04-reducer/TodoApp.jsx
+++ src/components/04-reducer/TodoApp.jsx
@@ -1,27 +1,100 @@
 import React, { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+import useForm from "../../hooks/useForm";
 
-const initialState = [
-  {
-    id: new Date().getTime(),
-    desc: "Estudiar React",
-    done: false,
-  },
-];
+import "./style.css";
+
+// const initialState = [
+//   {
+//     id: new Date().getTime(),
+//     desc: "Estudiar React",
+//     done: false,
+//   },
+// ];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
 
 const TodoApp = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
+  });
+  // console.log(todos);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+
+    // const action = {
+    //   type: "add",
+    //   payload: newTodo,
+    // };
+
+    dispatch({
+      type: "add",
+      payload: newTodo,
+    });
+
+    reset();
+  };
+
+  const borrarTodo = (todoId) => {
+    dispatch({
+      type: "delete",
+      payload: todoId,
+    });
+  };
 
   return (
     <div className="container">
-      <div className="row">
+      <div className="row mt-5">
         <div className="col">
           <h1>TodoApp (1)</h1>
           <hr />
         </div>
       </div>
       <div className="row">
-        <div className="col">todos</div>
-        <div className="col">agregar todos</div>
+        <div className="col-7">
+          {todos.map((todo, index) => (
+            <div className="card mb-3" key={todo.id}>
+              <div className="card-body d-flex justify-content-between align-items-center">
+                <p className="m-0">
+                  {index + 1}. {todo.desc}
+                </p>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => borrarTodo(todo.id)}
+                >
+                  X
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="col-5">
+          <h3>Agregar TODO</h3>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Ingrese la tarea..."
+              name="description"
+              value={description}
+              onChange={handleInputChange}
+            />
+            <div className="d-grid mt-3">
+              <button className="btn btn-outline-primary">Agregar</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,30 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// // import App from "./App";
-// // import RefDom from "./components/01-useRef/RefDom";
-// // import CasoUseRef from "./components/01-useRef/CasoUseRef";
-// // import Memorize from "./components/02-memo/Memorize";
-// // import MemoHook from "./components/02-memo/MemoHook";
-// // import CallbackHook from "./components/02-memo/CallbackHook";
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import RefDom from "./components/01-useRef/RefDom";
+// import CasoUseRef from "./components/01-useRef/CasoUseRef";
+// import Memorize from "./components/02-memo/Memorize";
+// import MemoHook from "./components/02-memo/MemoHook";
+// import CallbackHook from "./components/02-memo/CallbackHook";
 // import { Padre } from "./components/03-tarea-memo/Padre";
+import TodoApp from "./components/04-reducer/TodoApp";
 
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 
-// import "./index.css";
+import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     {/* <App /> */}
-//     {/* <RefDom /> */}
-//     {/* <CasoUseRef /> */}
-//     {/* <Memorize /> */}
-//     {/* <MemoHook /> */}
-//     {/* <CallbackHook /> */}
-//     <Padre />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    {/* <RefDom /> */}
+    {/* <CasoUseRef /> */}
+    {/* <Memorize /> */}
+    {/* <MemoHook /> */}
+    {/* <CallbackHook /> */}
+    {/* <Padre /> */}
+    <TodoApp />
+  </React.StrictMode>
+);
 
-import "./components/04-reducer/intro-reducer";
+// import "./components/04-reducer/intro-reducer";
